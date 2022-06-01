1988231ea2cb652a5285dd3d05747236df801375
Marco Bungart <marco.bungart@googlemail.com>
2022-06-01 21:44:52 +0200

    failure scenario for bug report

--- citrus/src/test/java/de/turing85/citrus/tests/citrus/configuration/ConfigurationRoot.java
+++ citrus/src/test/java/de/turing85/citrus/tests/citrus/configuration/ConfigurationRoot.java
@@ -4,6 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import({ HttpClients.class, Validators.class })
+@Import({ HttpClients.class/*, Validators.class*/ })
 public class ConfigurationRoot {
 }
