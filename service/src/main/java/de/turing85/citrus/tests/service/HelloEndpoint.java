4bab94e27777355ac7dcf71255c1bd92a5426b2f
Marco Bungart <marco.bungart@googlemail.com>
2023-05-29 02:51:25 +0200

    Add info output to service

--- service/src/main/java/de/turing85/citrus/tests/service/HelloEndpoint.java
+++ service/src/main/java/de/turing85/citrus/tests/service/HelloEndpoint.java
@@ -5,10 +5,16 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
 
 @Path("hello")
 @Produces(MediaType.TEXT_PLAIN)
 public class HelloEndpoint {
+    private static final Logger LOGGER = LoggerFactory.getLogger(HelloEndpoint.class);
+
     private final MyRestClient myRestClient;
 
     public HelloEndpoint(@RestClient MyRestClient myRestClient) {
@@ -16,7 +22,12 @@ public class HelloEndpoint {
     }
 
     @GET
-    public String hello() {
-        return myRestClient.getGreeting();
+    public String hello() throws InterruptedException {
+        Thread.sleep(Duration.ofSeconds(2).toMillis());
+        String greeting =  "";
+        if (LOGGER.isInfoEnabled()) {
+            LOGGER.info("sending \"%s\" as response".formatted(greeting));
+        }
+        return greeting;
     }
 }
